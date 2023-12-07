316195540696948468d3b3a4aad544944e4d1d97
Zhe Song <songzhewhu@gmail.com>
2023-12-07 00:21:06 -0500

    Optimize the import modules

--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
